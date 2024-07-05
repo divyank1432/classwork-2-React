1408bd304e63ddfc6204201b33357f67ea621e8b
divyank1432 <divyank.kumar1432@gmail.com>
2024-07-05 19:53:21 +0530

    cdn task

---
+++ script.js
@@ -0,0 +1,7 @@
+const p1 = React.createElement("div", {class : "font-mono"} , 'If you are a complete beginner, web development can be challenging - we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.')
+const anchor = React.createElement("a" ,{href : "#", class: "text-blue-500 textDecoration: underline"},"rest of MDN" )
+const p2 = React.createElement("div", {class : "font-mono"} , 'The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from the ',anchor,' and other intermediate to advanced resources that assume a lot of previous knowledge.')
+const p3 = React.createElement("div", {class : "font-mono" } ,'Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites.')
+const h1 = React.createElement("h1", {class : "text-center m-4 font-bold text-[20px]"} ,"Learn Web Development")
+const div = React.createElement("div", {class : ""} ,[h1 , p3 , p2 , p1])
+ReactDOM.render(div , document.getElementById("root"));
